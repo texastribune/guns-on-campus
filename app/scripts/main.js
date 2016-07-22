462f1c5e4a54385d1eb5864b71bc8784d528f3e7
becca <brickaa@users.noreply.github.com>
2016-07-22 10:20:29 -0500

    set up video page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,11 @@
+/* global $ */
+
+import './includes/fitvids';
+
 import './includes/adLoader';
 
+$('#comment__policy--link').click(function() {
+  $('#comment__policy--box').toggle();
+});
+
+$('#main').fitVids();
